94d1c0fc426882f9c8241ee01cd33c48afded36c
Nikolai Shegai <nixer1978@yahoo.com>
2017-02-21 11:57:01 -0800

    collapsable playlists

--- spoty_flask.py
+++ spoty_flask.py
@@ -45,16 +45,17 @@ def index():
             tracks_temp = {}
 
         switched_artist_dic = {artist_dic[k]: k for k in artist_dic}  # dictionary: {album_name: artist_name}
-        return switched_artist_dic, result_dic, images, urls
+        artist_list_nospace = {k:v.replace(" ", "").replace("'", "") for k, v in switched_artist_dic.items()}
+        return switched_artist_dic, result_dic, images, urls, artist_list_nospace
 
     tracks_2016 = get_playlist_tracks('12135627614', '4TTuo9NoDRKXSRPJvQQEaE')
     tracks_2017 = get_playlist_tracks('12135627614', '6eyCzm6cWR733Hdv0p5tCW')
-    switched_artist_dic_16, result_dic_16, images_16, urls_16 = get_tracks_dic(tracks_2016)
-    switched_artist_dic_17, result_dic_17, images_17, urls_17 = get_tracks_dic(tracks_2017)
+    switched_artist_dic_16, result_dic_16, images_16, urls_16, no_space_16 = get_tracks_dic(tracks_2016)
+    switched_artist_dic_17, result_dic_17, images_17, urls_17, no_space_17 = get_tracks_dic(tracks_2017)
     return render_template('index.html', rdic16=result_dic_16, imurl16=images_16,
-                           artist_dic_sw16=switched_artist_dic_16, urls16=urls_16,
+                           artist_dic_sw16=switched_artist_dic_16, urls16=urls_16, nsp16=no_space_16,
                            rdic17=result_dic_17, imurl17=images_17, artist_dic_sw17=switched_artist_dic_17,
-                           urls17=urls_17)
+                           urls17=urls_17, nsp17=no_space_17)
 
 
 if __name__ == '__main__':
